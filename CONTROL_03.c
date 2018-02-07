69a9588250026f14a9dd9b16319d38783c72f29f
samraeng temram <36057523+samraeng@users.noreply.github.com>
2018-02-07 09:51:09 +0700

    Update CONTROL_03.c

--- CONTROL_03.c
+++ CONTROL_03.c
@@ -1,4 +1,13 @@
-#include <test_program.h>
+#include <18F452.h>
+#device adc=16
+
+#FUSES NOWDT                    //No Watch Dog Timer
+#FUSES WDT128                   //Watch Dog Timer uses 1:128 Postscale
+#FUSES HS                       //High speed Osc (> 4mhz for PCM/PCH) (>10mhz for PCD)
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+
+#use delay(clock=10000000)
 
 //#define sw_prg pin_b0
 
